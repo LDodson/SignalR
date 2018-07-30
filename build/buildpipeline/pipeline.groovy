fa2afe372efc9232f80f7dd054226160b0757720
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-07-30 16:24:45 -0700

    Remove standard OS builds from Jenkins config
    We now have good enough coverage from VSTS for this. We're keeping the Spanish-language image because we don't have coverage there in VSTS.

--- build/buildpipeline/pipeline.groovy
+++ build/buildpipeline/pipeline.groovy
@@ -1,22 +1,10 @@
 import org.dotnet.ci.pipelines.Pipeline
 
-def windowsPipeline = Pipeline.createPipeline(this, 'build/buildpipeline/windows.groovy')
 def windowsESPipeline = Pipeline.createPipeline(this, 'build/buildpipeline/windows-es.groovy')
-def linuxPipeline = Pipeline.createPipeline(this, 'build/buildpipeline/linux.groovy')
-def osxPipeline = Pipeline.createPipeline(this, 'build/buildpipeline/osx.groovy')
 String configuration = 'Release'
 def parameters = [
     'Configuration': configuration
 ]
 
-windowsPipeline.triggerPipelineOnEveryGithubPR("Windows ${configuration} x64 Build", parameters)
-windowsPipeline.triggerPipelineOnGithubPush(parameters)
-
 windowsESPipeline.triggerPipelineOnEveryGithubPR("Windows ${configuration} Spanish Language x64 Build", parameters)
 windowsESPipeline.triggerPipelineOnGithubPush(parameters)
-
-linuxPipeline.triggerPipelineOnEveryGithubPR("Ubuntu 16.04 ${configuration} Build", parameters)
-linuxPipeline.triggerPipelineOnGithubPush(parameters)
-
-osxPipeline.triggerPipelineOnEveryGithubPR("OSX 10.12 ${configuration} Build", parameters)
-osxPipeline.triggerPipelineOnGithubPush(parameters)
